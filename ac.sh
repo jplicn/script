d6d543998d738e954d7d457b0df3f11d469663c7
jplicn <57996785+jplicn@users.noreply.github.com>
2023-12-17 22:39:10 +0800

    Update ac.sh

--- ac.sh
+++ ac.sh
@@ -36,13 +36,32 @@ read -p "请输入解析后的域名: " domain
 echo "$domain" > /root/domain.txt  # 将输入的域名写入文件
 echo "已输入的域名：$domain" && sleep 1
 
+# 提示用户选择证书机构
+echo "请选择证书机构:"
+echo "1. Let's Encrypt"
+echo "2. Buypass"
+echo "3. ZeroSSL"
+read -p "请输入选项: " ca_choice
+
+if [[ $ca_choice == "1" ]]; then
+    ca="letsencrypt"
+elif [[ $ca_choice == "2" ]]; then
+    ca="buypass"
+elif [[ $ca_choice == "3" ]]; then
+    ca="zerossl"
+else
+    # 如果选择其他机构，提示用户输入机构名称
+    read -p "请输入证书机构名称: " custom_ca
+    ca="$custom_ca"
+fi
+
 # 注册账号并使用随机生成的邮箱
-/root/.acme.sh/acme.sh --register-account -m "$random_email"
+/root/.acme.sh/acme.sh --register-account -m "$random_email" --server "$ca"
 
 # 开放 80 端口
 ufw allow 80
 
-/root/.acme.sh/acme.sh --issue -d "$domain" --standalone -k ec-256 --ca letsencrypt
+/root/.acme.sh/acme.sh --issue -d "$domain" --standalone -k ec-256 --ca "$ca"
 
 # 安装证书
 /root/.acme.sh/acme.sh --installcert -d "$domain" --ecc --key-file /root/server.key --fullchain-file /root/server.crt
