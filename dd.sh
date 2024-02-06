8d006a092973c77fc9e43128f9de657251499195
jplicn <57996785+jplicn@users.noreply.github.com>
2024-02-06 11:13:29 +0800

    Update dd.sh

--- dd.sh
+++ dd.sh
@@ -193,7 +193,7 @@ function selectMirror(){
   New=$(echo "$4" |sed 's/\ //g')
   [ -n "$Relese" ] && [ -n "$DIST" ] && [ -n "$VER" ] || exit 1
   if [ "$Relese" == "debian" ] || [ "$Relese" == "ubuntu" ]; then
-    [ "$DIST" == "jammy" ] && legacy="legacy-" || legacy=""
+    [ "$DIST" == "focal" ] && legacy="legacy-" || legacy=""
     TEMP="SUB_MIRROR/dists/${DIST}/main/installer-${VER}/current/${legacy}images/netboot/${Relese}-installer/${VER}/initrd.gz"
   elif [ "$Relese" == "centos" ]; then
     TEMP="SUB_MIRROR/${DIST}/os/${VER}/isolinux/initrd.img"
@@ -373,7 +373,7 @@ if [[ -n "$tmpDIST" ]]; then
         [[ "$isDigital" == '14.04' ]] && DIST='trusty';
         [[ "$isDigital" == '16.04' ]] && DIST='xenial';
         [[ "$isDigital" == '18.04' ]] && DIST='bionic';
-        [[ "$isDigital" == '20.04' ]] && DIST='jammy';
+        [[ "$isDigital" == '20.04' ]] && DIST='focal';
         [[ "$isDigital" == '22.04' ]] && DIST='jammy';
       }
     }
@@ -461,7 +461,7 @@ fi
 echo -e "\n[\033[33m$Relese\033[0m] [\033[33m$DIST\033[0m] [\033[33m$VER\033[0m] Downloading..."
 
 if [[ "$linux_relese" == 'debian' ]] || [[ "$linux_relese" == 'ubuntu' ]]; then
-  [ "$DIST" == "jammy" ] && legacy="legacy-" || legacy=""
+  [ "$DIST" == "focal" ] && legacy="legacy-" || legacy=""
   wget --no-check-certificate -qO '/tmp/initrd.img' "${LinuxMirror}/dists/${DIST}/main/installer-${VER}/current/${legacy}images/netboot/${linux_relese}-installer/${VER}/initrd.gz"
   [[ $? -ne '0' ]] && echo -ne "\033[31mError! \033[0mDownload 'initrd.img' for \033[33m$linux_relese\033[0m failed! \n" && exit 1
   wget --no-check-certificate -qO '/tmp/vmlinuz' "${LinuxMirror}/dists/${DIST}${inUpdate}/main/installer-${VER}/current/${legacy}images/netboot/${linux_relese}-installer/${VER}/linux"
