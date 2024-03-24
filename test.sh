55c6c539d17300515a03b1e0c2bcaaa0900ce2ee
jplicn <57996785+jplicn@users.noreply.github.com>
2024-03-24 20:10:50 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -420,6 +420,32 @@ cat > /root/sbox/sbconfig_server.json << EOF
     "level": "info",
     "timestamp": true
   },
+      "dns": {
+    "servers": [
+      {
+        "tag": "cloudflare",
+        "address": "https://1.1.1.1/dns-query",
+        "strategy": "ipv4_only",
+        "detour": "direct"
+      },
+      {
+        "tag": "block",
+        "address": "rcode://success"
+      }
+    ],
+    "rules": [
+      {
+        "rule_set": [
+          "geosite-category-ads-all"
+        ],
+        "server": "block"
+      }
+    ],
+    "final": "cloudflare",
+    "strategy": "",
+    "disable_cache": false,
+    "disable_expire": false
+  },
   "inbounds": [
     {
         "type": "hysteria2",
